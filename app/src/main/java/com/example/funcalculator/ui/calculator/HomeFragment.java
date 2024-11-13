d4df6f62e6a8989cd50adf2403ca1dd479c5afc5
Vrillault Gabin <gabin.vrillault@ecole.ensicaen.fr>
2024-11-13 11:03:33 +0100

    Responsive Game design

--- app/src/main/java/com/example/funcalculator/ui/calculator/HomeFragment.java
+++ app/src/main/java/com/example/funcalculator/ui/calculator/HomeFragment.java
@@ -1,10 +1,9 @@
-package com.example.funcalculator.ui.home;
+package com.example.funcalculator.ui.calculator;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
